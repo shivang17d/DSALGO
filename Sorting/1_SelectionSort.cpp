575f2e2ee1d3fd74fc94b7298f7545d3c3a0732c
Shivang Gupta <86548591+sgdevelop@users.noreply.github.com>
2021-09-09 20:16:38 +0530

    Create 1_SelectionSort.cpp

---
+++ Sorting/1_SelectionSort.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+class Sort
+{
+public:
+    int n, arr[];
+    void read_data();
+    void swap(int *x, int *y);
+    void selectionSort();
+    void display_data();
+};
+void Sort ::read_data()
+{
+    cout << "Enter the size of the array : " << endl;
+    cin >> n;
+    cout << "Enter the array : " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+}
+void Sort ::swap(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+void Sort ::selectionSort()
+{
+    int min;
+    for (int i = 0; i < n - 1; i++)
+    {
+        min = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min])
+            {
+                min = j;
+            }
+        }
+        if (min != i)
+        {
+            swap(&arr[i], &arr[min]);
+        }
+    }
+}
+void Sort ::display_data()
+{
+    cout << "The sorted array is : " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+int main()
+{
+    Sort array;
+    array.read_data();
+    array.selectionSort();
+    array.display_data();
+    return 0;
+}
