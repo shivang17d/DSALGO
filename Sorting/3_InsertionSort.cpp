7a4e435148c88f2e73afaec1a6f1bf8e0d4a382b
Shivang Gupta <86548591+sgdevelop@users.noreply.github.com>
2021-09-09 23:02:35 +0530

    Create 3_InsertionSort.cpp

---
+++ Sorting/3_InsertionSort.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+class Sort
+{
+public:
+    int n, arr[];
+    void read_data();
+    void insertionSort();
+    void display_data();
+};
+void Sort ::read_data()
+{
+    cout << "Enter the size of the array : " << endl;
+    cin >> n;
+    cout << "Enter the array : " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+}
+void Sort ::insertionSort()
+{
+    int temp, j;
+    for (int i = 1; i < n; i++)
+    {
+        temp = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+    }
+}
+void Sort ::display_data()
+{
+    cout << "The sorted array is : " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+int main()
+{
+    Sort array;
+    array.read_data();
+    array.insertionSort();
+    array.display_data();
+    return 0;
+}
+
+
+// Time Complexity
+// Best    : O(n)
+// Average : O(n^2)
+// Worst   : O(n^2)
