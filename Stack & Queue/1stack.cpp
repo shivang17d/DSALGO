fdf70d7d78241ce72c0016a2e5473964de672ff3
Shivang Gupta <86548591+sgdevelop@users.noreply.github.com>
2021-10-05 18:55:43 +0530

    Update 1stack.cpp

--- Stack & Queue/1stack.cpp
+++ Stack & Queue/1stack.cpp
@@ -5,6 +5,7 @@ class Stack
 {
     int top;
     int arr[MAX];
+
 public:
     Stack()
     {
@@ -19,44 +20,44 @@ public:
 };
 bool Stack ::isEmpty()
 {
-    return(top < 0);
+    return (top < 0);
 }
 bool Stack ::isFull()
 {
-    return(top >= MAX-1);
+    return (top >= MAX - 1);
 }
 void Stack ::push(int x)
 {
     if (isFull())
     {
-        cout << "Stack Overflow"<<endl;
+        cout << "Stack Overflow" << endl;
     }
     else
     {
         top++;
         arr[top] = x;
-        cout << x << " pushed into stack"<<endl;
+        cout << x << " pushed into stack" << endl;
     }
 }
 int Stack::pop()
 {
     if (isEmpty())
     {
-        cout << "Stack Underflow"<<endl;
+        cout << "Stack Underflow" << endl;
         return 0;
     }
     else
     {
         int popValue = arr[top];
         top--;
-        return popValue;   
+        return popValue;
     }
 }
 int Stack::peek()
 {
     if (top < 0)
     {
-        cout << "Stack is Empty"<<endl;
+        cout << "Stack is Empty" << endl;
         return 0;
     }
     else
@@ -68,46 +69,42 @@ int Stack::peek()
 
 int main()
 {
-    class Stack s;
-    if(s.isEmpty())
+    Stack s;
+    if (s.isEmpty())
     {
-        cout<<"Stack is Empty"<<endl;
+        cout << "Stack is Empty" << endl;
     }
     s.peek();
     s.push(23);
-    cout<<"Peak Value is "<<s.peek()<<endl;
+    cout << "Peak Value is " << s.peek() << endl;
     s.push(11);
-    cout<<"Peak Value is "<<s.peek()<<endl;
+    cout << "Peak Value is " << s.peek() << endl;
     s.push(30);
-    cout<<"Peak Value is "<<s.peek()<<endl;
+    cout << "Peak Value is " << s.peek() << endl;
     s.push(10);
-    cout<<"Peak Value is "<<s.peek()<<endl;
+    cout << "Peak Value is " << s.peek() << endl;
     s.push(80);
-    cout<<"Peak Value is "<<s.peek()<<endl;
-    if(s.isFull())
+    cout << "Peak Value is " << s.peek() << endl;
+    if (s.isFull())
     {
-        cout<<"Stack is Full"<<endl;
+        cout << "Stack is Full" << endl;
     }
     s.push(70);
     s.push(21);
     s.push(10);
 
-    
+    cout << s.pop() << " Popped from stack" << endl;
+    cout << "Peak Value is " << s.peek() << endl;
 
-    cout << s.pop() << " Popped from stack"<<endl;
-    cout<<"Peak Value is "<<s.peek()<<endl;
+    cout << s.pop() << " Popped from stack" << endl;
+    cout << "Peak Value is " << s.peek() << endl;
 
-    cout << s.pop() << " Popped from stack"<<endl;
-    cout<<"Peak Value is "<<s.peek()<<endl;
-    
-
-
-    cout<<"Elements present in stack : ";
-    while(!s.isEmpty())
+    cout << "Elements present in stack : ";
+    while (!s.isEmpty())
     {
-        cout<<s.peek()<<" ";
+        cout << s.peek() << " ";
         s.pop();
     }
- 
+
     return 0;
 }
